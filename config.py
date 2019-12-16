4b65a048405c57f9c6a779edb33845c7eb47f5be
ankitp <ankitp@ridlr.in>
2019-12-16 23:12:04 +0530

    config file

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+dataset_path = './dataset/'
+log_path = './logs'
+
+train_batch_size = 128
+validation_batch_size = 256
+block_size = 24
+block_channels = 3
+train_stride = 10
+validation_stride = 32
+jpeg_quality = 0.6
+
+number_of_train_iterations = 1000
+learning_rate = 0.0008
--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 
 from arcnn import ARCNN
+import config
 
 def main():
-    dataset_path = './dataset/'
-    log_path = './logs'
     
-    train_batch_size = 128
-    validation_batch_size = 256
-    block_size = 32
-    block_channels = 3
-    train_stride = 10
-    validation_stride = 32
-    jpeg_quality = 0.6
-    
-    number_of_train_iterations = 1000
-    learning_rate = 0.0006
-
-    arcnn = ARCNN(dataset_path, log_path, jpeg_quality, block_size, block_channels, train_batch_size,
-                  train_stride, validation_batch_size, validation_stride)
-    arcnn.build_model(learning_rate)
-    arcnn.train_model(number_of_train_iterations)
+    arcnn = ARCNN(config.dataset_path, config.log_path, config.jpeg_quality, config.block_size,
+                  config.block_channels, config.train_batch_size, config.train_stride,
+                  config.validation_batch_size, config.validation_stride)
+    arcnn.build_model(config.learning_rate)
+    arcnn.train_model(config.number_of_train_iterations)
     return
 
 if __name__=='__main__':
